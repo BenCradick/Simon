9e8b35cc410eb9dff946b1b6a1d1de332fed190c
bencradick <ben.cradick@gmail.com>
2019-03-05 07:43:48 -0600

    second commit, hopefully rest of files tracked

---
+++ app/src/main/java/com/example/simon/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.simon
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import kotlinx.android.synthetic.main.activity_main.view.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val difficultySpinner : Spinner = findViewById(R.id.difficulty_spinner)
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.difficulties_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            difficultySpinner.adapter = adapter
+        }
+
+    }
+
+
+
+}
